f167462d137357d8fc926817e03a8c7a13528e76
pabgo <pabgora94@gmail.com>
2017-12-09 18:08:53 -0600

    Add files via upload

---
+++ estimador_sistema/estimacion_top/sistema_estimacion/control_flujo_datos_estimacion/FSM_control_flujo_datos .v
@@ -0,0 +1,131 @@
+﻿`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Pablo Gomez Ramirez
+// 
+// Create Date: 04/18/2017 12:44:17 AM
+// Design Name: 
+// Module Name: FSM_control_flujo_datos
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FSM_control_flujo_datos
+(
+	input wire CLK,RESET,ACK_I,ACK_V,ACK_E,ACK_THETA_I,ACK_THETA_V, ACK_CAS_I,ACK_CAS_V,
+	output reg START_I, START_V, START_E, START_THETA_I, START_THETA_V, CLEAR, REG_THETA_I, REG_THETA_V
+);
+//*********************************************************
+
+localparam [2:0] // Codificaci�n de los estados o etiquetas
+	state_0 = 3'b000,
+	state_1 = 3'b001,
+	state_2 = 3'b010,
+	state_3 = 3'b011,
+	state_4 = 3'b100;
+
+reg [3:0] state, next_state; // Reg, estado actual y siguiente
+
+//*********************************************************
+
+//Parte Secuencial
+
+always@(posedge CLK, posedge RESET)
+begin
+	if(RESET)
+    begin
+		state <= state_0;
+	    //START_I = 1'b1;// se agregan para darle arranque a la maquina.
+        //START_V = 1'b1;// de otra manera la máquina no funciona
+        //CLEAR = 1'b1;
+    end
+	else
+		state <= next_state;
+end
+
+//*********************************************************
+
+//Parte Combinacional
+always@*
+begin
+	next_state = state;
+	START_I = 0;
+	START_V = 0;
+	START_E = 0;
+	START_THETA_I = 0;
+	START_THETA_V = 0;
+	REG_THETA_I = 0;
+	REG_THETA_V = 0;
+	CLEAR = 0;
+
+
+	case(state)
+        state_0:
+            if(((ACK_CAS_I)&&(ACK_CAS_V))==1)
+            begin
+                CLEAR = 1'b1;
+                REG_THETA_I = 1'b0;
+                REG_THETA_V = 1'b0;
+                //START_V = 1'b1;
+                next_state = state_1;
+            end
+	
+	    state_1:
+		    begin
+                CLEAR = 1'b0;
+                START_I = 1'b1;
+                START_V = 1'b1;
+                next_state = state_2;
+            end	    
+	        
+		state_2:
+			if(((ACK_I)&&(ACK_V))==1)
+			begin
+			    //START_I = 1'b1;
+			    //START_V = 1'b1;
+			    START_E = 1'b1; //se agrega para darle arranque al estimador. Al parecer no necesita esta señal
+                next_state = state_3;
+             end
+             
+
+		state_3:
+            if((ACK_E)==1)
+            begin
+                //START_E = 1'b1;
+                START_THETA_I = 1'b1; // se agregan para darle arranque al siguiente modulo
+                START_THETA_V = 1'b1;
+                next_state = state_4;
+            end						
+		
+		state_4:
+            if(((ACK_THETA_I)&&(ACK_THETA_V))==1)
+            begin
+                REG_THETA_I = 1'b1;
+                REG_THETA_V = 1'b1;
+                //CLEAR = 1'b1;
+                next_state = state_0;
+            end
+
+//		state_5:
+//		    begin
+//                next_state = state_0;
+//		    end
+		    
+		default:
+			//CLEAR = 1'b0; 
+			next_state = state_0;
+			
+	endcase
+end
+
+endmodule
---
+++ estimador_sistema/estimacion_top/sistema_estimacion/deslinealizador_desnormalizador_fijo_flotante/CORDIC-Exponential-Function/Verilog/Multiplier/DESNORMALIZADOR.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.03.2016 12:07:25
+// Design Name: 
+// Module Name: NORMALIZADOR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module NORMALIZADOR(
+    input wire [31:0] A, //entrada al normalizador 
+    output wire [31:0] Y //salida de la normalizacion en coma fija, coma entre el bit 30 y 29
+    );
+
+//wire [61:0] Out;      
+wire [63:0] Out;      
+    
+    MULTIPLIER_32BITS_L MULT_NORM(
+        .A(A),
+        //.B(32'b00000001110010011110000110110000),
+        //.B(32'b00000000110011000111000100001100),//0.199641     1/valor maximo constante de adrian
+        //.B(32'b00000010111000111111111011111011),//0.72265236     constante de JJ
+        .B(32'b00000100000000000000000000000000), // 1   valor prueba sin normalizacion
+        .Y(Out)
+        );
+
+//assign Y = Out[54:22]; //constante de adrian   
+//assign Y = Out[57:26];   //constante  de jj
+assign Y = {Out[57],{3{1'b0}},Out[56:52],Out[51:29]};   //constante  de jj y ajuste para estimador a formato 1:8:23
+
+
+endmodule
